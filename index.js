527f8fe166351b0dca09ccb88d1873c8eff44b49
Sk-Kalesha <kaleemuddeen.shaik@gmail.com>
2022-11-09 05:51:55 +0000

    React-CP-1-Congrats-Card

---
+++ index.js
@@ -0,0 +1,21 @@
+const element = (
+  <div class="main-container">
+    <h1 className="heading">Congratulations</h1>
+    <div class="sub-container">
+      <img
+        class="image"
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+      ></img>
+      <h1 className="name-heading">Kiran V</h1>
+      <p className="para">
+        Vishnu Institue of Computer Education and technology and Bhimavaram
+      </p>
+      <img
+        class="image"
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+      ></img>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
